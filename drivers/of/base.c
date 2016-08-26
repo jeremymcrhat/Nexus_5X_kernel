57839fff463bdbcd9950ff60835b3877d9ce4555
Jeremy McNicoll <jmcnicol@redhat.com>
2016-08-26 03:32:20 -0700

    Remove extra verbose print

--- drivers/of/base.c
+++ drivers/of/base.c
@@ -984,7 +984,6 @@ const struct of_device_id *of_match_node(const struct of_device_id *matches,
 	raw_spin_lock_irqsave(&devtree_lock, flags);
 	match = __of_match_node(matches, node);
 	raw_spin_unlock_irqrestore(&devtree_lock, flags);
-printk("%s: compat-> %s node: %s  \n", __func__, matches->compatible, node->name);
 
 	return match;
 }
